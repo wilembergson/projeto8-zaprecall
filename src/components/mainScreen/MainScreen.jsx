969d3582a94cdab94efbedee0519fdffda2bba97
wilembergson <wilembergson@yahoo.com>
2022-03-17 12:01:36 -0300

    MainScreen in progress

--- src/components/mainScreen/MainScreen.jsx
+++ src/components/mainScreen/MainScreen.jsx
@@ -1,7 +1,26 @@
 import './MainScreen.css'
+import { deck1 } from '../../decksData/DecksData.js'
 
 export default function MainScreen(){
+    const deck = [...deck1]
+    deck.sort(comparator)
+
     return(
-        <div className="mainScreen">Tela Principal</div>
+        <div className="mainScreen">
+            <header>
+                <img src="images/logo-pequeno.png"/>
+                <text>ZapRecall</text>
+            </header>
+
+           {deck.map(e => 
+               <div>{e.ask}</div>
+           )}
+
+            <footer>0/4 CONCLUÍDOS</footer>
+        </div>
     )
+}
+
+function comparator() { 
+	return Math.random() - 0.5; 
 }
---
+++ src/decksData/DecksData.js
@@ -0,0 +1,18 @@
+export const deck1 = [
+    {
+        ask: "Seu projeto está pronto?",
+        answer: "Ainda não."
+    },
+    {
+        ask: "Tá confiante?",
+        answer: "Na medida do possível."
+    },
+    {
+        ask: "Vai se comparar com os outros?",
+        answer: "Vou tentar não fazer isso."
+    },
+    {
+        ask: "No final, vai dar bom?",
+        answer: "Tem que dar."
+    }
+]
